2499c642e75567af37fd6a6c0ed355acecdfd2be
Jigar Kakadiya <jigar.kakadiya@prama.ai>
2023-04-04 17:45:50 +0530

    initial commit

---
+++ src/main/java/com/example/githubwebhook/UserController.java
@@ -0,0 +1,25 @@
+package com.example.githubwebhook;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public String user() {
+        return "Hello User!";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "welcome to home!";
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return Arrays.asList("a", "b", "c").toString();
+    }
+}
