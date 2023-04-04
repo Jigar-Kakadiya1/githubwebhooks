7be55aa6774a6300ac700d8f4d4d0c60014cd340
Jigar Kakadiya <jigar.kakadiya@prama.ai>
2023-04-04 19:07:51 +0530

    some chnages

--- src/main/java/com/example/githubwebhook/UserController.java
+++ src/main/java/com/example/githubwebhook/UserController.java
@@ -10,7 +10,7 @@ public class UserController {
 
     @GetMapping("/user")
     public String user() {
-        return "Hello User!";
+        return "Hello User! welcome";
     }
 
     @GetMapping("/")
